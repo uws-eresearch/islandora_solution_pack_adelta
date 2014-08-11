67c3a6f83f4d3eb790cb1e5feb47db4d8ec39c1d
Lloyd <l.harischandra@uws.edu.au>
2014-08-11 14:48:11 +1000

    uws-eresearch/ADELTA#62 - contain images within the viewport

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,6 @@
  */
 (function($) {
 	$(document).ready(function () {
-        $('a.img_gallery').colorbox({ photo:true, opacity:0.5, rel:'img_gallery' });
+        $('a.img_gallery').colorbox({ photo:true, opacity:0.5, rel:'img_gallery', maxWidth:'100%', maxHeight:'100%' });
     });
 })(jQuery);
